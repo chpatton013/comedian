3003c5a6c5b7021ebd53aca79530f522cd454723
Christopher Patton <chpatton013@gmail.com>
2020-06-06 17:03:59 -0700

    Add context to command api

--- comedian/command.py
+++ comedian/command.py
@@ -1,16 +1,41 @@
-from abc import ABC, abstractmethod
-from typing import Iterator
+"""
+Command API for generating shell commands to be run on a system.
+"""
 
+from abc import ABC, abstractmethod
+from typing import Iterator, List
+
+from .configuration import Configuration
+from .graph import Graph
 from .traits import __Debug__, __Eq__
 
 
 class Command(__Debug__, __Eq__):
-    def __init__(self, cmd: str, cwd: str = None):
+    """
+    A container for the arguments and CWD of a shell command.
+    """
+    def __init__(self, cmd: List[str], cwd: str = None):
         self.cmd = cmd
         self.cwd = cwd
 
 
+class CommandContext(__Debug__):
+    """
+    A structure holding the arguments necessary for generating Commands.
+    """
+    def __init__(
+        self,
+        config: Configuration,
+        graph: Graph,
+    ):
+        self.config = config
+        self.graph = graph
+
+
 class CommandGenerator(ABC):
+    """
+    Abstract base class for a Callable that generates a series of Commands.
+    """
     @abstractmethod
-    def generate_commands(self) -> Iterator[Command]:
+    def generate_commands(self, context: CommandContext) -> Iterator[Command]:
         pass
