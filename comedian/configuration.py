887f1208ae55c22a2136afb0b526c1c951eced8c
Christopher Patton <chpatton013@gmail.com>
2020-06-06 20:37:35 -0700

    Fix absolute path joining

--- comedian/configuration.py
+++ comedian/configuration.py
@@ -17,7 +17,14 @@ class Configuration(__Debug__, __Eq__):
         self.tmp_dir = tmp_dir
 
     def media_path(self, path: str) -> str:
-        return os.path.join(self.media_dir, path)
+        return _join(self.media_dir, path)
 
     def tmp_path(self, path: str) -> str:
-        return os.path.join(self.tmp_dir, path)
+        return _join(self.tmp_dir, path)
+
+
+def _join(head, *tail):
+    joined = _join(*tail) if tail else ""
+    if joined.startswith(os.path.sep):
+        joined = joined[len(os.path.sep):]
+    return head + os.path.sep + joined if joined else head
